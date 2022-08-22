fd36ae2765dcbacc1987438f444460c47684c9f8
David Vera Castillo <indigodavid@gmail.com>
2022-08-22 16:01:21 -0500

    Fix Linters errors

--- lib/school_library/person.rb
+++ lib/school_library/person.rb
@@ -2,18 +2,18 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize (age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @age = age
     @name = name
     @parent_permission = parent_permission
-    @id = Random.rand(1...100000)
+    @id = Random.rand(1...100_000)
   end
 
-  def is_of_age?
-    return @age >= 18
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
-    return self.is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 end
