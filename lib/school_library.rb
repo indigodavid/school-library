e9894bf417ae7d814f5839b59cd6efd250ddfe66
David Vera Castillo <indigodavid@gmail.com>
2022-08-23 15:19:18 -0500

    Test decorator patterns

--- lib/school_library.rb
+++ lib/school_library.rb
@@ -2,10 +2,14 @@ require_relative 'school_library/version'
 require_relative 'school_library/person'
 require_relative 'school_library/teacher'
 require_relative 'school_library/student'
+require_relative 'school_library/capitalize_decorator'
+require_relative 'school_library/trimmer_decorator'
 
 module SchoolLibrary
   class Error < StandardError; end
   # Your code goes here...
+
+  # test base classes
   p1 = Person.new(18, 'Fabian')
   t1 = Teacher.new(35, 'Computer Science', 'David')
   s1 = Student.new(17, '101', 'Leonardo', parent_permission: false)
@@ -21,4 +25,12 @@ module SchoolLibrary
   p s1.can_use_services?
 
   p s1.play_hooky
+
+  # test decorators
+  person = Person.new(22, 'maximilianus')
+  p person.correct_name
+  capitalizedPerson = CapitalizeDecorator.new(person)
+  p capitalizedPerson.correct_name
+  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+  p capitalizedTrimmedPerson.correct_name
 end
