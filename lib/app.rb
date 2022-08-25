07f50e222859b65b0b5b3bade1835bc0b9207ad3
David Vera Castillo <indigodavid@gmail.com>
2022-08-25 15:14:57 -0500

    Add Create Book and Create Rental functionality

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,6 @@
 require_relative 'school_library/student'
 require_relative 'school_library/teacher'
+require_relative 'school_library/book'
 require_relative 'school_library/rental'
 require_relative 'school_library/classroom'
 
@@ -10,23 +11,22 @@ class App
     @people = []
     @books = []
     @rentals = []
-    @classroom = ('Default Class')
+    @classroom = 'Default Class'
   end
 
   def list_books
-    if @books.length > 0
-      books.each {|book| puts "Title: #{book.title}, Author: #{book.author}"}
+    if @books.length.positive?
+      books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
     else
       puts 'No books added.'
     end
   end
 
   def list_people
-    if @people.length > 0
-      people.each {|person|
-        puts "[Student] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" if person.is_a?(Student)
-        puts "[Teacher] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" if person.is_a?(Teacher)
-      }
+    if @people.length.positive?
+      people.each do |person|
+        puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      end
     else
       puts 'No people added.'
     end
@@ -35,10 +35,24 @@ class App
   def create_student(age, name, parent_permission)
     student = Student.new(age, @classroom, name, parent_permission: parent_permission)
     @people.push(student)
+    puts 'Student created successfully'
   end
 
   def create_teacher(age, specialization, name)
     teacher = Teacher.new(age, specialization, name)
     @people.push(teacher)
+    puts 'Teacher created successfully'
+  end
+
+  def create_book(title, author)
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'Book created successfully'
+  end
+
+  def create_rental(date, person_index, book_index)
+    rental = Rental.new(date, @people[person_index], @books[book_index])
+    @rentals.push(rental)
+    puts 'Rental created successfully'
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,68 +1,102 @@
 require_relative 'app'
 
-def list_books(app)
-  app.list_books()
+def print_options
+  puts ''
+  puts 'Please choose and option by entering a number:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all the rentals for a given person id'
+  puts '7 - Exit'
 end
 
-def list_people(app)
-  app.list_people()
-end
-
-def create_person(app)
-  p 'Do you want to create a Student (1) or a Teacher (2)? [Input the number]:'
-  type = gets.chomp
-  p 'Age: '
-  age = gets.chomp.to_i
-  p 'Name: '
-  name = gets.chomp
-  if type == '1'
-    p 'Has parent permission? [Y/N]: '
-    parent_permission = gets.chomp
-    app.create_student(age, name, parent_permission: parent_permission.downcase == 'y')
-    puts 'Student created successfully'
-  elsif type == '2'
-    p 'Specialization: '
-    specialization = gets.chomp
-    app.create_teacher(age, specialization, name)
-    puts 'Teacher created successfully'
-  else
-    'Input error. Going back to menu...'
+def call_actions(option, app)
+  case option
+  when 1
+    list_books(app)
+  when 2
+    list_people(app)
+  when 3
+    create_person(app)
+  when 4
+    create_book(app)
+  when 5
+    create_rental(app)
+  when 6
+    puts 'List rentals'
   end
 end
 
+def list_books(app)
+  app.list_books
+end
+
+def list_people(app)
+  app.list_people
+end
+
+def create_person(app)
+  puts 'Do you want to create a Student (1) or a Teacher (2)? [Input the number]:'
+  type = gets.chomp.to_i
+  puts 'Age: '
+  age = gets.chomp.to_i
+  puts 'Name: '
+  name = gets.chomp
+  case type
+  when 1
+    puts 'Has parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    app.create_student(age, name, parent_permission: parent_permission.downcase == 'y')
+  when 2
+    puts 'Specialization: '
+    specialization = gets.chomp
+    app.create_teacher(age, specialization, name)
+  else
+    puts 'Input error. Returning to menu...'
+  end
+end
+
+def create_book(app)
+  puts 'Title: '
+  title = gets.chomp
+  puts 'Author: '
+  author = gets.chomp
+  app.create_book(title, author)
+end
+
+def create_rental(app)
+  puts 'Select a book from the following list by number:'
+  app.books.each_with_index {
+    |book, index|
+    puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
+  }
+  book_index = gets.chomp.to_i
+
+  puts 'Select a person from the following list by number (not id):'
+  app.people.each_with_index {
+    |person, index|
+    puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+  }
+  person_index = gets.chomp.to_i
+
+  puts 'Date: '
+  date = gets.chomp
+
+  app.create_rental(date, person_index, book_index)
+end
+
 def main
-  app = App.new()
+  app = App.new
   option = 0
   puts 'Welcome to School Library Rental App!'
-  while option != '7'
-    puts ''
-    puts 'Please choose and option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all the rentals for a given person id'
-    puts '7 - Exit'
-
-    option = gets.chomp
-    case option
-    when '1'
-      puts '## Books ##'
-      list_books(app)
-    when '2'
-      puts '## People ##'
-      list_people(app)
-    when '3'
-      puts '## Create person ##'
-      create_person(app)
-    when '4'
-      puts 'Create book'
-    when '5'
-      puts 'Create rental'
-    when '6'
-      puts 'List rentals'
-    when '7'
+  while option != 7
+    print_options
+    option = gets.chomp.to_i
+    if option.positive? && option < 7
+      call_actions(option, app)
+    elsif option == 7
       puts 'Thanks for using this App!'
     else
       puts 'Invalid option'
@@ -70,4 +104,4 @@ def main
   end
 end
 
-main()
+main
