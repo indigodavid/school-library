751ce87b945359f102788f907567d0c8722bbb9a
David Vera Castillo <indigodavid@gmail.com>
2022-08-30 15:59:34 -0500

    Fix Rental class initialize method

--- lib/school_library/rental.rb
+++ lib/school_library/rental.rb
@@ -3,15 +3,15 @@ require_relative 'person'
 
 class Rental
   attr_accessor :date
-  attr_reader :person, :book
+  attr_reader :person, :book 
 
   def initialize(date, person, book)
     @date = date
 
     @person = person
-    @person.rentals << self
+    @person.rentals << (self)
 
     @book = book
-    @book.rentals << self
+    @book.rentals << (self)
   end
 end
