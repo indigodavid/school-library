844e006047e0a9c1f5c58db29c14622cf6f27bed
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-08-31 16:45:50 -0300

    Add test for capitalize_decorator class

---
+++ lib/school_library/spec/capitalize_decorator_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../person'
+require_relative '../capitalize_decorator'
+
+describe CapitalizeDecorator do
+  context "When testing the capitalize_decorator class" do
+    before :each do
+      @person = Person.new(30, 'david')
+      @capitalize_decorator = CapitalizeDecorator.new(@person)
+    end
+
+    it "The capitalize_decorator should not be null" do
+      expect(@capitalize_decorator).to be_truthy
+    end
+
+    it "The capitalize_decorator's correct_name should be David" do
+      expect(@capitalize_decorator.correct_name).to eq('David')
+    end
+  end
+end
